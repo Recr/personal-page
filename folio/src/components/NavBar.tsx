f2d9e356927e539dfa84e3e0eb22c8372e9afa0d
Recr <126016150+Recr@users.noreply.github.com>
2025-03-20 23:28:41 -0300

    feat: add tailwind, react router and first page

---
+++ folio/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return(
+    <nav>
+      <h1>a</h1>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ folio/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+      <div className="h-screen bg-gray-900 font-dmmono">
+        <h1 className="text-2xl text-gray-200">Hello visitor. I am</h1>
+        <h1 className="text-7xl text-gray-200">Eliel Silva</h1>
+        <h2 className="text-5xl text-blue-500">&gt; A developer</h2>
+      </div>
+  )
+}
+
+export default Home
